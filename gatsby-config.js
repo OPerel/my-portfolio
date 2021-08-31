a55f40c7cffe031e25868a2cfa5d911e1cea3c26
Operel <oriperelman@gmail.com>
2021-08-31 10:13:19 +0300

    try again with loadable

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,8 @@ module.exports = {
     },
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-loadable-components-ssr`,
+    `gatsby-plugin-webpack-bundle-analyser-v2`,
+    `gatsby-plugin-loadable-components-ssr`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
