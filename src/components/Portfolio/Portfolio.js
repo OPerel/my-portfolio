437d946dd86a28ca52ec5fe922bac98c05a18268
Operel <oriperelman@gmail.com>
2021-10-09 16:08:44 +0300

    add mediaQuery in Portfolio.js

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,17 +1,15 @@
 import React from 'react';
-// import Gallery from './Gallery/Gallery';
 import Parallax from '../Parallax/Parallax';
 import './Portfolio.scss';
-// import { useHasBeenVisible } from "../../hooks/useIsLoaded"
-
 import loadable from '@loadable/component';
-// import useIsLoaded from "../../hooks/useIsLoaded"
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 const Gallery = loadable(() => import('./Gallery/Gallery'));
-// const Gallery = React.lazy(() =>
-//   import('./Gallery/Gallery')
-// );
+
+
 
 const Portfolio = ({ animeClass, projects, scrolled }) => {
+  const matches = useMediaQuery('(min-width: 768px)');
+
   return (
     <section className="app-portfolio">
       <div className="container">
@@ -19,7 +17,7 @@ const Portfolio = ({ animeClass, projects, scrolled }) => {
       </div>
       <Parallax
         animeClass={animeClass}
-        positions={{ on: 5, over: -50, under: 60 }}
+        positions={{ on: matches ? 5 : 0, over: -50, under: 60 }}
       >
         {scrolled && <Gallery projects={projects} />}
       </Parallax>
